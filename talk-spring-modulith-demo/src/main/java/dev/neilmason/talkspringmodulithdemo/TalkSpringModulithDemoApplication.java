df1bbfa93db2163fdcaacba79e080b9d8b1b95eb
neilpmas <neilpmas@gmail.com>
2026-05-06 14:56:24 +1000

    Set up initial Spring Boot Modulith application with Maven Wrapper:
    - Added Maven Wrapper files for consistent build execution
    - Created the `pom.xml` with dependencies for Spring Boot and Modulith
    - Added application.properties with application name
    - Implemented a basic Spring Boot application entry point
    - Included a basic JUnit test to validate context loading

---
+++ talk-spring-modulith-demo/src/main/java/dev/neilmason/talkspringmodulithdemo/TalkSpringModulithDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.neilmason.talkspringmodulithdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSpringModulithDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TalkSpringModulithDemoApplication.class, args);
+    }
+
+}
---
+++ talk-spring-modulith-demo/src/test/java/dev/neilmason/talkspringmodulithdemo/TalkSpringModulithDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.neilmason.talkspringmodulithdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkSpringModulithDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
